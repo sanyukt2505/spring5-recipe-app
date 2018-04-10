0134fb24a295729794c8747082be4c656a6147c9
ssharma <ssharma@ebates.com>
2018-04-09 22:24:27 -0700

    assignment

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -7,52 +7,50 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by jt on 6/19/17.
  */
-@Slf4j
-@Controller
-public class RecipeController {
+@Slf4j @Controller public class RecipeController {
 
     private final RecipeService recipeService;
 
-    public RecipeController(RecipeService recipeService) {
+    public RecipeController(final RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
-    @GetMapping("/recipe/{id}/show")
-    public String showById(@PathVariable String id, Model model){
+    @GetMapping("/recipe/{id}/show") public String showById(@PathVariable final String id, final Model model) {
 
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
     }
 
-    @GetMapping("recipe/new")
-    public String newRecipe(Model model){
+    @GetMapping("recipe/new") public String newRecipe(final Model model) {
         model.addAttribute("recipe", new RecipeCommand());
 
         return "recipe/recipeform";
     }
 
-    @GetMapping("recipe/{id}/update")
-    public String updateRecipe(@PathVariable String id, Model model){
+    @GetMapping("recipe/{id}/update") public String updateRecipe(@PathVariable final String id, final Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-        return  "recipe/recipeform";
+        return "recipe/recipeform";
     }
 
-    @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
-        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+    @PostMapping("recipe") public String saveOrUpdate(@ModelAttribute final RecipeCommand command) {
+        final RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
-    @GetMapping("recipe/{id}/delete")
-    public String deleteById(@PathVariable String id){
+    @GetMapping("recipe/{id}/delete") public String deleteById(@PathVariable final String id) {
 
         log.debug("Deleting id: " + id);
 
@@ -60,18 +58,30 @@ public class RecipeController {
         return "redirect:/";
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception){
+    @ResponseStatus(HttpStatus.NOT_FOUND) @ExceptionHandler(NotFoundException.class) public ModelAndView handleNotFound(
+            final Exception exception) {
 
         log.error("Handling not found exception");
         log.error(exception.getMessage());
 
-        ModelAndView modelAndView = new ModelAndView();
+        final ModelAndView modelAndView = new ModelAndView();
 
         modelAndView.setViewName("404error");
         modelAndView.addObject("exception", exception);
 
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST) @ExceptionHandler(NumberFormatException.class) public ModelAndView handleBadRequest(
+            final Exception exception) {
+
+        log.error("Handling Number Format exception");
+        log.error(exception.getMessage());
+
+        final ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
 
         return modelAndView;
     }
